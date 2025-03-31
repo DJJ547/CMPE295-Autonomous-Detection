e39684c36211a172deca667072b70d47f88a4b90
Haoming <hmchen85451@gmail.com>
2025-03-31 14:02:01 -0700

    added car markers

--- backend/routes/test.py
+++ backend/routes/test.py
@@ -17,16 +17,5 @@ def get_markers():
         {"class": "graffiti", "lat": "37.7741", "lng": "-122.4144"},
     ]
     
-    # Simulate random increase of markers
-    num_new_markers = random.randint(1, 5)  # Random number of markers to add (1 to 5)
-    
-    for _ in range(num_new_markers):
-        # Generate a random marker
-        new_marker = {
-            "class": random.choice(["road-damage", "encampment", "graffiti"]),  # Random class
-            "lat": str(round(random.uniform(37.77, 37.78), 4)),  # Random latitude in range 37.7 to 37.8
-            "lng": str(round(random.uniform(-122.41, -122.43), 4)),  # Random longitude in range -122.5 to -122.3
-        }
-        base_markers.append(new_marker)
     
     return jsonify(base_markers)
