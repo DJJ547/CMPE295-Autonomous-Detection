d359c823396f72c4bbbdffbff45b073cccd2de06
Rachel Fan <fanrg@mail.uc.edu>
2025-04-28 12:44:30 -0700

    update loadspinner

--- frontend/src/components/common/loadspinner.js
+++ frontend/src/components/common/loadspinner.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const LoadingSpinner = () => {
   return (
-    <div className="flex justify-center items-center h-48">
+    <div className="flex flex-col justify-center items-center h-48 space-y-4">
       <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-blue-500 border-solid"></div>
+      <p className="text-gray-600 text-lg font-medium">Loading chart...</p>
     </div>
   );
 };
