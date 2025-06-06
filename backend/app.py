9ab28bf98379991d0c0f84f9b761e30d7c645594
Haoming <hmchen85451@gmail.com>
2025-06-05 18:15:22 -0700

    worked on database json for llm

--- backend/app.py
+++ backend/app.py
@@ -56,4 +56,4 @@ import routes.stream_socket
 
 # Run the app
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=8000)
+    socketio.run(app, host='0.0.0.0', port=8000, debug=True)
--- backend/routes/llm.py
+++ backend/routes/llm.py
@@ -6,6 +6,7 @@ import numpy as np
 from flask import Blueprint, jsonify, request
 from google.generativeai import types
 import google.generativeai as genai
+import json
 
 load_dotenv()
 
@@ -25,8 +26,48 @@ def generate():
 
     # Initialize or retrieve chat history
     if user_id not in conversations:
-        model = genai.GenerativeModel("gemini-pro")
+        model = genai.GenerativeModel("gemini-2.0-flash")
         chat = model.start_chat(history=[])
         conversations[user_id] = chat
     else:
         chat = conversations[user_id]
+    
+    response = chat.send_message(
+        prompt,
+        generation_config={
+            "max_output_tokens": 200
+        }
+    )
+    return jsonify({"reply": response.text})
+
+from mysql_models import DetectionEvent, DetectionImage, DetectionMetadata
+from sqlalchemy.orm import joinedload
+
+
+@llm_bp.route('/api/llm-summarize', methods=['POST'])
+def summarize():
+    events = DetectionEvent.query.options(
+        joinedload(DetectionEvent.images).joinedload(DetectionImage.metadatas)
+    ).all()
+    
+    anomalyJson = []
+    for event in events:
+        anomalyList = []
+        for image in event.images:
+            anomalyList.extend([metadata.type.value for metadata in image.metadatas])
+                        
+        anomalyJson.append(
+            {
+                "anamolyTypes": anomalyList,
+                "latitude": str(event.latitude),
+                "longitude": str(event.longitude),
+                "timestamp": event.timestamp.isoformat(),
+                "street": event.street,
+                "city": event.city,
+                "state": event.state,
+                "zipcode": event.zipcode,
+            })
+    json_string = json.dumps(anomalyJson, indent=2)  # `indent=2` for pretty print
+    print(json_string)    
+        
+    return jsonify({"reply": anomalyJson})
